1d9183aeb64062d28f8219e939427c4a2b9a58b2
LordPottingtin <32198503+LordPottingtin@users.noreply.github.com>
2018-11-30 23:59:47 -0600

    changing back to normal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,9 +54,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
                             </li>
-                            <li class='nav-item'>
-                                <a class='nav-link' href="{{ route('reports') }}">{{ __('Reports') }}</a>
-                            </li>
                             <?php
                             if(Auth::check()){
                             if(Auth::user()->admin == 1)
